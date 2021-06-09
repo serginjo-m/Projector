7a8bc56d74af948f525837e57a3155c3ea921d2c
serginjo-m <melnik_serii@mail.ru>
2021-06-09 18:12:54 +0200

    File management

---
+++ Projector/Main VC/QuickNotesCollViews/CanvasNotesVC.swift
@@ -0,0 +1,9 @@
+//
+//  CanvasCVViewController.swift
+//  Projector
+//
+//  Created by Serginjo Melnik on 09.06.2021.
+//  Copyright © 2021 Serginjo Melnik. All rights reserved.
+//
+
+import Foundation
