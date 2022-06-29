93e7c9964312158cfd905e410079235f1b5af1df
serginjo-m <serginjoredline@gmail.com>
2022-06-29 18:06:55 +0200

    just app delegate

--- Projector/AppDelegate.swift
+++ Projector/AppDelegate.swift
@@ -21,9 +21,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         Realm.Configuration.defaultConfiguration = Realm.Configuration(
-            schemaVersion: 31,
+            schemaVersion: 36,
             migrationBlock: { migration , oldSchemaVersion in
-                if oldSchemaVersion < 31 {
+                if oldSchemaVersion < 36 {
                     
                 }
             }
